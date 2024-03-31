b57398d7023847934763cf7bdbcee2e5168205a1
JeongHoon <cjh41820@gmail.com>
2024-03-31 11:26:40 +0900

    콘솔에식나 표시

--- Assets/Samples/Tobii XR SDK/3.0.1/Developer Tools/Scripts/HighlightAtGaze.cs
+++ Assets/Samples/Tobii XR SDK/3.0.1/Developer Tools/Scripts/HighlightAtGaze.cs
@@ -15,18 +15,35 @@ namespace Tobii.XR.Examples.DevTools
         private Renderer _renderer;
         private Color _originalColor;
         private Color _targetColor;
+        private float focusStartTime; // 포커스 시작 시간을 기록할 변수
+        private float focusEndTime; // 포커스 종료 시간을 기록할 변수
 
         //The method of the "IGazeFocusable" interface, which will be called when this object receives or loses focus
         public void GazeFocusChanged(bool hasFocus)
         {
+            
             //If this object received focus, fade the object's color to highlight color
             if (hasFocus)
             {
+                if (_targetColor != highlightColor)
+                {
+                    focusStartTime = Time.time;
+                }
                 _targetColor = highlightColor;
             }
             //If this object lost focus, fade the object's color to it's original color
             else
             {
+                focusEndTime = Time.time;
+                float focusDurationInSeconds = focusEndTime - focusStartTime;
+                if (focusDurationInSeconds >= 0)
+                {
+                    Debug.Log("Focus duration: " + focusDurationInSeconds + " seconds");
+                }
+                else
+                {
+                    Debug.LogWarning("Invalid focus duration: " + focusDurationInSeconds + " seconds. Possible event order issue.");
+                }
                 _targetColor = _originalColor;
             }
         }
@@ -36,12 +53,11 @@ namespace Tobii.XR.Examples.DevTools
             _renderer = GetComponent<Renderer>();
             _originalColor = _renderer.sharedMaterial.color;
             _targetColor = _originalColor;
+            
         }
 
         private void Update()
         {
-            var eyeTrackingTime = TobiiXR.GetEyeTrackingData(TobiiXR_TrackingSpace.World).Timestamp;
-            Debug.Log(eyeTrackingTime);
             //This lerp will fade the color of the object
             if (_renderer.sharedMaterial.HasProperty(_baseColor)) // new rendering pipeline (lightweight, hd, universal...)
             {
