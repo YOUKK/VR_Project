4464603aab78ef28a9c5fbd802d44b8c09dd6562
yyj <ycwon13@naver.com>
2024-05-19 13:01:05 +0900

    ui 수정

--- Assets/Scripts/JJH_Scripts/Tutorial.cs
+++ Assets/Scripts/JJH_Scripts/Tutorial.cs
@@ -18,7 +18,12 @@ public class Tutorial : MonoBehaviour
     public GameObject RightRayInteractor;
     public GameObject TutorialStuff;
     public GameObject ButtonStuff;
-    public GameObject ResultStuff;
+    //public GameObject ResultStuff;
+
+    public GameObject ResultSuccess;
+    public GameObject ResultFail;
+
+
     int clickCnt = 0;
     bool ButtonFlag = false;
 
@@ -33,11 +38,11 @@ public class Tutorial : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        ScriptTxt.text = "안녕하세요! 저는 당신을 위한 특별한 도우미입니다.\n오늘은 우리 함께 재미있는 미션을 수행해 볼 거에요.";
-        ButtonTxt1.text = "버튼1";
-        ButtonTxt2.text = "버튼2";
-        ButtonTxt3.text = "버튼3";
-        ButtonTxt4.text = "버튼4";
+        ScriptTxt.text = "안녕하세요!\n저는 당신을 위한 특별한 도우미예요.\n오늘은 재미있는 미션을 수행해 볼 거에요.";
+        ButtonTxt1.text = "부엌에서 그릇을\n찬장으로 옮기기";
+        ButtonTxt2.text = "부엌에서 음식을\n준비하기";
+        ButtonTxt3.text = "부엌에서 식사 도구\n정리하기";
+        ButtonTxt4.text = "부엌을 청소하기";
 
         CountTxt.text = countdownTime.ToString();
         CountBackground.SetActive(false);
@@ -55,21 +60,24 @@ public class Tutorial : MonoBehaviour
             switch (clickCnt)
             {
                 case 1:
-                    ScriptTxt.text = "지시사항에 따라 다양한 미션을 수행하게 될 것입니다.";
+                    ScriptTxt.text = "지시사항에 따라 다양한 미션을\n수행하게 될 거예요.";
                     break;
                 case 2:
-                    ScriptTxt.text = "미션은 물건 정리와 물건 탐색으로 구성되어 있습니다.";
+                    ScriptTxt.text = "미션은 물건 정리와 물건 분류로\n구성되어 있어요.";
                     break;
                 case 3:
-                    ScriptTxt.text = "그럼 준비가 되었다면 우리 함께 시작해 볼까요?";
+                    ScriptTxt.text = "첫 번째 미션은\n[부엌 안에서 식사 도구 정리하기]예요.";
                     break;
                 case 4:
-                    ScriptTxt.text = "질문";
+                    ScriptTxt.text = "그럼 준비가 되었다면\n우리 함께 시작해 볼까요?";
+                    break;
+                case 5:
+                    ScriptTxt.text = "여기서 잠깐!\n미션을 이해했는지 확인하려고 해요. \n첫 번째 미션이 무엇인지 선택해주세요.";
                     ButtonFlag = true;
                     ButtonStuff.SetActive(true);
                     break;
-                case 5:
-                    ScriptTxt.text = "정답";
+                case 6:
+                    ScriptTxt.text = "정답입니다.\n이제 시작해볼까요?";
                     break;
                 default:
                     LeftRayInteractor.SetActive(false);
@@ -89,21 +97,21 @@ public class Tutorial : MonoBehaviour
 
     public void Button1()
     {
-        if(clickCnt == 4)
+        if(clickCnt == 5)
         {
-            ScriptTxt.text = "질문\n오답입니다 다시한번 생각해보세요";
+            ScriptTxt.text = "오답입니다.\n다시한번 생각해보세요";
         }
     }
     public void Button2()
     {
-        if (clickCnt == 4)
+        if (clickCnt == 5)
         {
-            ScriptTxt.text = "질문\n오답입니다 다시한번 생각해보세요";
+            ScriptTxt.text = "오답입니다.\n다시한번 생각해보세요";
         }
     }
     public void Button3()
     {
-        if (clickCnt == 4)
+        if (clickCnt == 5)
         {
             ButtonFlag = false;
             ButtonStuff.SetActive(false);
@@ -112,9 +120,9 @@ public class Tutorial : MonoBehaviour
     }
     public void Button4()
     {
-        if (clickCnt == 4)
+        if (clickCnt == 5)
         {
-            ScriptTxt.text = "질문\n오답입니다 다시한번 생각해보세요";
+            ScriptTxt.text = "오답입니다.\n다시한번 생각해보세요";
         }
     }
     void ChangeImage()
@@ -143,19 +151,23 @@ public class Tutorial : MonoBehaviour
                 StartCoroutine(ShakeText(0.5f, 0.1f));
             }
 
-            if (GameObject.Find("dish-drainer").GetComponent<OrganizeDish>().currentTurn == 1 &&
+            if (GameObject.Find("dish-drainer").GetComponent<OrganizeDish>().currentTurn == 2 &&
                 GameObject.Find("knife-block").GetComponent<OrganizeKnife>().currentTurn == 0 &&
                 GameObject.Find("Sink").GetComponent<OrganizeSpoonFork>().currentTurn == 0)
             {
-                ResultStuff.SetActive(true);
+                ResultSuccess.SetActive(true);
                 Debug.Log("성공");
                 Debug.Log(countdownTime);
+                CountBackground.SetActive(false);
+                CountTxt.gameObject.SetActive(false);
                 break;
             }
             if (countdownTime == 0)
             {
-                ResultStuff.SetActive(true);
+                ResultFail.SetActive(true);
                 Debug.Log("실패");
+                CountBackground.SetActive(false);
+                CountTxt.gameObject.SetActive(false);
 
             }
             
